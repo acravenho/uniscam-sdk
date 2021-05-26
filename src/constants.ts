ad9b2eed8303f9afa50863a794fe1a94d0332477
Andrew Cravenho <andrew@cravenho.me>
2021-05-25 23:35:37 -0400

    update init code

--- src/constants.ts
+++ src/constants.ts
@@ -34,7 +34,7 @@ export const FACTORY_ADDRESS = '0x5757371414417b8C6CAad45bAeF941aBc7d3Ab32'
 export const INIT_CODE_HASH_BSC = '0x9b025805045f6da539a4926f67307a9b0ab5aaccc42d3f14aaea0776a5b727ac'
 export const INIT_CODE_HASH_ETH = '0xa852da7194a3edaba2a5a4545af11dda00ca97d89e0f9934bb841dffaa964220'
 export const INIT_CODE_HASH_HECO = '0x7b9686f044376bf1a04de2e9f1dbb6c9a4fe516f7cbd89b68be801982b62f913'
-export const INIT_CODE_HASH_MATIC = '0xf523ab2e058386ecc391c92c3cda3906b9c8a50c2cca82c4f12ea030fee83af1'
+export const INIT_CODE_HASH_MATIC = '0xbab145d02e7005f0d84c6c1639d39b799b0ea16df99ebbdaf5a14d9da820b4e0'
 // @XXX: change this if factory was replaced
 export const INIT_CODE_HASH_OKCHAIN_TESTNET = '0x85c3c9a75ba81d6e35a53711a7eb5cb910409d3396d00129d9b7006d76dbc310'
 export const INIT_CODE_HASHES: InitCodeHashes = {
